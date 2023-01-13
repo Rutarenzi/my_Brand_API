93a4e4427afea114d9ff626a643ca400285a7634
Rutagarma Axcel <88782915+Rutarenzi@users.noreply.github.com>
2023-01-13 09:23:55 +0200

    Update swagger.js

--- src/swagger.js
+++ src/swagger.js
@@ -29,6 +29,15 @@ const options = {
           description: 'Dev server'
       }
     ]
+     components: {
+      securitySchemes: {
+        bearerAuth: {
+          type: "http",
+          scheme: "bearer",
+          bearerFormat: "JWT",
+        },
+      },
+    },
   },
   // Paths to files containing OpenAPI definitions
   apis: ['./src/routes/*.js'],
