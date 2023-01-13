1ae33faab1b0c3542c8cadb924a7ef3b1b49473f
Rutagarama Axcel <rutagaramaaxcel@gmail.com>
2023-01-13 07:32:37 +0200

    --a

--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -5,6 +5,32 @@ import  jwt  from "jsonwebtoken";
 
 const router = Router();
 
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *      User:
+ *        type: object
+ *        properties:
+ *          _id:
+ *            type: string
+ *            format: objectId
+ *            example: 63a14e44f08ce9c0f90689fe
+ *          name:
+ *            type: string
+ *            example: Rugarama axcel
+ *          email:
+ *            type: string
+ *            format: email
+ *            example: ruta@gmail.com
+ *          password:
+ *            type: string
+ *            format: password
+ *            example: 12345678
+ *          __v:
+ *            type: integer
+ *            example: 0
+ */
 
 router.post('/signup',passport.authenticate('signup', { session: false }),
     async (req, res, done) => {
@@ -14,6 +40,44 @@ router.post('/signup',passport.authenticate('signup', { session: false }),
       });
     }
 );
+/**
+ * @swagger
+ * /signup:
+ *   post:
+ *     tags:
+ *       - Users
+ *     summary: Sign up 
+ *     description: Signing up
+ *     operationId: Signup
+ *     requestBody:
+ *         description: you are required to provide email,password
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 email:
+ *                   type: string
+ *                   format: email
+ *                   example: ngsol@gmail.com
+ *                 password:
+ *                   type: string
+ *                   format: password
+ *                   example: andela
+ *               required:
+ *                 - name
+ *                 - email
+ *                 - password
+ *     responses:
+ *       "200":
+ *           description: Success
+ *       '401':
+ *         description: Unauthorized
+ *       "409":
+ *           description: Conflict
+ *       '500':
+ *         description: Internal server error
+ */
 
 
 
@@ -43,7 +107,39 @@ router.post('/login',async (req, res, next) =>{
       )(req, res, next);
     }
 );
-
+/**
+ * @swagger
+ *   /login:
+ *   post:
+ *     tags:
+ *       - Users
+ *     summary: Login in 
+ *     description: Log in
+ *     operationId: login
+ *     requestBody:
+ *         description: provide  email and password to login
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 email:
+ *                   type: string
+ *                   format: email
+ *                   example: ruta@gmail.com
+ *                 password:
+ *                   type: string
+ *                   format: password
+ *                   example: 12345678
+ *               required:
+ *                 - email
+ *                 - password
+ *     responses:
+ *       "200":
+ *           description: Success
+ *       '500':
+ *           description: Internal server error
+ */
 
 export default router
 
